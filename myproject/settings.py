21edfbbfac1b76ad020ac3823d9e2ca87abc51f4
scottmmoore176 <scottmmoore176@gmail.com>
2025-05-16 00:21:06 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-rrnjoxp-3=-g5xhz_nlgt$a+j)w1go0#o((^voff!10e9_p#lp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ali-page.onrender.com', '127.0.0.1' ]
+ALLOWED_HOSTS = ['https://ali-page.onrender.com', '127.0.0.1', 'ali-page.onrender.com' ]
 
 
 # Application definition
